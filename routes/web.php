9f1672c3d1ec61c745593f3a30ba80668dd661f2
Renan <rmgomes@prefeitura.sp.gov.br>
2025-10-13 08:39:53 -0300

    feat: Estrutura tela de validacao

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    $nome = $_SESSION['nomeUsuario'] ?? 'Visitante';
+    $rf = $_SESSION['IDUsuario'];
+
+    return view('welcome', compact('nome', 'rf'));
 });
