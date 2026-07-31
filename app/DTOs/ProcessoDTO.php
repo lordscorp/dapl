bf374b69b7872a15c00257713dd89b077992a9ea
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2026-07-30 21:34:30 -0300

    refactor: ProcessoDTO: adiciona 'situacao', substitui 'sql' por 'setor' e 'quadra'

--- app/DTOs/ProcessoDTO.php
+++ app/DTOs/ProcessoDTO.php
@@ -7,9 +7,12 @@ readonly class ProcessoDTO
     public function __construct(
         public string $processoSei,
         public string $dataAutuacao,
+        public string $situacao,
+        public string $sistema,
         public string $protocolo,
         public string $dataProtocolo,
-        public string $sqlIncra,
+        public string $setor,
+        public string $quadra,
         public string $codlog
     ) {}
 }
